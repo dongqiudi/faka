d53bd4fe178c0ff965219d6714506cd6a9c14ae4
dongqiudi <102015501+dongqiudi@users.noreply.github.com>
2022-10-25 20:53:07 +0800

    Update keys.js

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI: "mongodb://127.0.0.1:27017/faka",
+    mongoURI: "mongodb+srv://vercel-admin-user:wNrbtbI3x2qOPPlo@cluster0.1o55ys0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     serectOrKey: "screct",
     payjskey: "xxx"
 }
